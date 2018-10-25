099d4a9838f9f501c55ffd96cf1cf598d84648a6
mburq <mburq@mit.edu>
2018-10-25 12:31:15 -0700

    Passes basic tests in Julia 1.0

--- src/match.jl
+++ src/match.jl
@@ -2,14 +2,14 @@
 ## Find maximum matching in the graph
 ###################################################
 
-function match(graph::DiGraph,
+function kidney_match(graph::DiGraph,
                weights::Array{Float64,2};
                verbose::Int64 = 0,
                techno::String = "cycles", # can be "cycles", "chains", "cycles_chains"
                max_cycle_length::Int64 = 2,
                chain_max_length::Int64 = 0,
                solver::String = "gurobi",
-               ndds::Array{Int64, 1} = Array{Int64}(0))
+               ndds::Array{Int64, 1} = zeros(Int64, 0))
 
   time_model_init = time()
   m = init_model(solver)
@@ -31,7 +31,8 @@ function match(graph::DiGraph,
             two_three_cycles_constraint(m, n, graph, x)
             status = solve(m)
         else
-            error("Techno $(techno), with $(max_cycle_length) cycles and $(chain_max_length) is not yet implemented")
+            error("Techno $(techno), with $(max_cycle_length) cycles and
+                  $(chain_max_length) is not yet implemented")
         end
     elseif techno == "chains"
         if solver == "gurobi"
@@ -57,10 +58,9 @@ function match(graph::DiGraph,
             end
         end# chain_constraint(m, n, graph, x, in, out, ndds)
     else
-        error("Techno $(techno), with $(max_cycle_length) cycles and \
-            $(chain_max_length) is not yet implemented")
+        error("Techno $(techno), with $(max_cycle_length) cycles and
+              $(chain_max_length) is not yet implemented")
     end
-
 
   # push!(l.time_jump, time() - time_model_init)
   # println(m)
@@ -123,7 +123,7 @@ function two_cycles_constraint(m::JuMP.Model, n::Int64, graph::DiGraph,
 end
 
 function check_cycle_length(x_val, n, max_cycle_length, graph)
-    flag = zeros(Array{Int64}(n))
+    flag = zeros(Int64, n)
     for i in 1:n
         if flag[i] == 0
             current_vertex = i
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,29 +8,28 @@ add_edge!(g1, 1,3)
 add_edge!(g1, 3,4)
 add_edge!(g1, 4,1)
 # l = Log(1, 1, false)
-w1 = ones(Array{Float64}(4, 4))
+w1 = ones(Float64, 4, 4)
 
-
-v, e, val = match(g1, w1, techno = "cycles", max_cycle_length = 2, solver = "gurobi")
+v, e, val = kidney_match(g1, w1, techno = "cycles", max_cycle_length = 2, solver = "gurobi")
 @test v == [1,1,0,0]
 @test val == 2
 
-v, e, val = match(g1, w1, techno = "cycles", max_cycle_length = 3, solver = "gurobi")
+v, e, val = kidney_match(g1, w1, techno = "cycles", max_cycle_length = 3, solver = "gurobi")
 @test v == [1,0,1,1]
 @test val == 3
 
-v, e, val = match(g1, w1, techno = "chains", chain_max_length = 3,
-             max_cycle_length = 0, solver = "GLPK", ndds=[3])
+v, e, val = kidney_match(g1, w1; techno = "chains", chain_max_length = 3,
+             max_cycle_length = 0, solver = "GLPK", ndds= 3 * ones(Int64, 1))
 @test v == [1,1,0,1]
 @test val == 3
 
-v, e, val = match(g1, w1, techno = "chains", chain_max_length = 3,
-             max_cycle_length = 0, solver = "gurobi", ndds=Array{Int64}(0))
+v, e, val = kidney_match(g1, w1, techno = "chains", chain_max_length = 3,
+             max_cycle_length = 0, solver = "gurobi")
 @test v == [0,0,0,0]
 @test val == 0
 
-v, e, val = match(g1, w1, techno = "chains", chain_max_length = 3,
-             max_cycle_length = 0, solver = "GLPK", ndds=Array{Int64}(0))
+v, e, val = kidney_match(g1, w1, techno = "chains", chain_max_length = 3,
+             max_cycle_length = 0, solver = "GLPK")
 @test v == [0,0,0,0]
 @test val == 0
 
@@ -40,6 +39,6 @@ add_edge!(g2, 2,1)
 add_edge!(g2, 1,3)
 add_edge!(g2, 1,4)
 
-v, e, val = match(g2, w1, techno = "cycles", max_cycle_length = 3, solver = "gurobi")
+v, e, val = kidney_match(g2, w1, techno = "cycles", max_cycle_length = 3, solver = "gurobi")
 @test v == [1,1,0,0]
 @test val == 2
